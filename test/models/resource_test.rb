e2d479557a5d8d9ab062d91c669a5c1afe5507d0
aivic-unigedesk <ali.ivic88@gmail.com>
2015-02-04 06:14:30 +0100

    Adds complementary message for the test case

--- test/models/resource_test.rb
+++ test/models/resource_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class ResourceTest < ActiveSupport::TestCase
   test "should not save resource without node_id" do
     resource = Resource.new
-    assert_not resource.save
+    assert_not resource.save, "The new resource with the provided node_id was saved"
   end
   test "should not save resource without hardware" do
     resource = Resource.new
-    assert_not resource.save
+    assert_not resource.save, "The new resource with the provided hardware was saved"
   end
   test "should not save resource without hostname" do
     resource = Resource.new
-    assert_not resource.save
+    assert_not resource.save, "The new resource with the provided hostname was saved"
   end
 end
